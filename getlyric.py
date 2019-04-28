300bbcd3ec80a6a13ff6dd42aae0c5cffe00460c
sshwy <jy.cat@qq.com>
2019-04-28 20:03:49 +0800

    小改动

--- getlyric.py
+++ getlyric.py
@@ -5,21 +5,22 @@ import os
 from mess import *
 from headers import *
 
-with open("./config.json",'r') as load_f:
-    cfg=json.load(load_f)
+with open("./config.json", 'r') as load_f:
+    cfg = json.load(load_f)
 
 save_path = cfg['lyric_save_path']
 
 if not os.path.exists(save_path):
     os.mkdir(save_path)
 
-def get_lrc(name,song_id):
-    lrc_url = 'http://music.163.com/api/song/lyric?' + 'id=' + str(song_id) + '&lv=1&kv=1&tv=-1'
-    lyric = requests.get(lrc_url,headers=headers)
+
+def get_lrc(name, song_id):
+    lrc_url = 'http://music.163.com/api/song/lyric?' + \
+        'id=' + str(song_id) + '&lv=1&kv=1&tv=-1'
+    lyric = requests.get(lrc_url, headers=headers)
     j = json.loads(lyric.text)
-    if not 'lrc' in j:
+    if 'lrc' not in j:
         console.warn(name+"没有歌词!")
         return
-    lrc = j['lrc']['lyric']
-    with open(save_path+".lyric/"+name+'.lrc','w') as f:
+    with open(save_path+".lyric/"+name+'.lrc', 'w') as f:
         f.write(j['lrc']['lyric'])
--- headers.py
+++ headers.py
@@ -1,6 +1,7 @@
 headers = {
     'Referer': 'http://music.163.com/',
     'Host': 'music.163.com',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.3\
+        6 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
 }
